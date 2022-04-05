4169e1ef15ac46d49981244b1e94409034d87cfc
leon.muchoki@thejitu.com <leon.muchoki@thejitu.com>
2022-04-05 17:44:30 +0300

    endpoint for organization tenders

--- routes/tender.routes.js
+++ routes/tender.routes.js
@@ -1,8 +1,8 @@
 const controller = require("../controllers/tender.controller");
 const { authJwt } = require("../middleware");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
@@ -15,9 +15,16 @@ module.exports = function(app) {
     [authJwt.verifyToken, authJwt.isContractingAuthorityOrAdmin],
     controller.createTender
   );
-  
+
   app.get("/api/tenders", controller.getAllTenders);
-  app.get("/api/category/tenders/:tenderCategoryId", controller.findCategoryTenders);
+  app.get(
+    "/api/category/tenders/:tenderCategoryId",
+    controller.findCategoryTenders
+  );
   app.get("/api/sector/tenders/:sectorId", controller.findSectorTenders);
   app.post("/api/tender/views/:tenderId", controller.updateViews);
+  app.get(
+    "/api/tender/ca/:businessId",
+    controller.findContractingAuthorityTenders
+  );
 };
