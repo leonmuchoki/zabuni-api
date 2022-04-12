cb9d24aee2aaa512fc951e136f6093b4593aef97
leon.muchoki@thejitu.com <leon.muchoki@thejitu.com>
2022-04-12 16:28:22 +0300

    add resolved field for issue model

--- models/issue.model.js
+++ models/issue.model.js
@@ -3,6 +3,10 @@ module.exports = (sequelize, DataTypes) => {
     const Issue = sequelize.define("issues", {
       description: {
         type: DataTypes.STRING
+      },
+      is_resolved: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
       }
     });
     return Issue;
