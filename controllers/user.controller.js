8797d5ca0aaa77bb7122789225e3a764dd060bb3
leon.muchoki@thejitu.com <leon.muchoki@thejitu.com>
2022-04-26 16:24:34 +0300

    sort add user bug

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 const User = db.user;
 const Role = db.role;
+const Op = db.Sequelize.Op;
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 const { sendEmail } = require("../utilities/sendEmail")
