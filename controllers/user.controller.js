8f533a22218dab3ac793bc197a7caf60519f629f
leon.muchoki@thejitu.com <leon.muchoki@thejitu.com>
2022-04-26 15:12:49 +0300

    sort create user bug

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 const db = require("../models");
 const User = db.user;
+const Role = db.role;
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 const { sendEmail } = require("../utilities/sendEmail")
