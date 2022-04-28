dc87beb7986f3ae15eef30e3514e0248b9e4b3fd
leon.muchoki@thejitu.com <leon.muchoki@thejitu.com>
2022-04-28 08:11:20 +0300

    sort ca dashboard stats endpoint bug

--- controllers/caDashboard.controller.js
+++ controllers/caDashboard.controller.js
@@ -1,18 +1,24 @@
 const db = require("../models");
 const Tender = db.tender;
 const User = db.user;
+const Issue = db.issue;
 
 exports.getContractingAuthorityDashboardStats =async(req, res)=> {
-    const businessId = req.params.businessId;
-    const staff = User.findAll({where: {companyId: businessId}});
-    const tenders = await Tender.findAll({where: {businessId: businessId}});
-    const issues = await Issue.findAll({where: {userId: req.userId}});
+    try {
+        const businessId = req.params.businessId;
+        const staff = User.findAll({where: {companyId: +businessId}});
+        const tenders = await Tender.findAll({where: {businessId: +businessId}});
+        const issues = await Issue.findAll({where: {userId: req.userId || 0}});
+        
+        const stats = {
+            issuesCount: issues.length || 0,
+            staffCount: staff.length || 0,
+            tendersCount: tenders.length || 0,
+        };
+        return res.status(200).send({stats});
+    } catch(ex) { 
+        return res.status(400).send({ex: JSON.stringify(ex)});
+    }
     
-    const stats = {
-        issuesCount: issues.length || 0,
-        staffCount: staff.length || 0,
-        tendersCount: tenders.length || 0,
-    };
     
-    return res.status(200).send({stats});
 }
